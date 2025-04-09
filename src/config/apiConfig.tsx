b732ae3a890e6627b7ab6ffbcde3a7a2b5e3b36b
ChiNg22nd04 <dedonai99@gmail.com>
2025-04-09 23:28:53 +0700

    update ui appointment

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -3,11 +3,15 @@
 export const API_BASE_URL = "http://localhost:3500";
 
 export const API_ENDPOINTS = {
+    common: {
+        home: "/home",
+    },
     auth: {
         login: "/login",
     },
     user: {
         scheduleAppointment: "/user/schedule-appointment",
     },
+
     // Thêm các endpoint khác nếu cần
 };
--- src/pages/customer/Appointment/ScheduleAppointment.tsx
+++ src/pages/customer/Appointment/ScheduleAppointment.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Header from "../../../components/Header/Header";
-
+import "./ScheduleAppointment.scss";
 const ScheduleAppointment: React.FC = () => {
     return (
         <div className="app">
             <Header />
-            <h1>Đặt lịch khám</h1>
+            <div className="container">
+                <div className="content-top">
+                    <h1 className="text-main">Đăng ký khám bệnh</h1>
+                    <ul className="text-sub">
+                        Quý khách hàng có nhu cầu đặt hẹn khám tại <span className="text-highlight">Hệ thống Bệnh viện Đa khoa Piedmont</span>, xin vui lòng thực hiện theo hướng dẫn:
+                    </ul>
+                    <li>
+                        Đặt hẹn bằng cách gọi tổng đài Chăm sóc khách hàng tại số <span className="text-highlight">0287 102 6789 – 093 180 6858</span> (Bệnh viện Đa khoa Piedmont TPHCM) hoặc{" "}
+                        <span className="text-highlight"> 024 3872 3872 – 024 7106 6858</span> (Bệnh viện Đa khoa Piedmont Hà Nội)
+                    </li>
+                    <li>Đặt hẹn trực tuyến bằng cách điền thông tin vào mẫu bên dưới.</li>
+                    <li>
+                        Xin lưu ý, trong các trường hợp khẩn cấp, quý khách vui lòng đến ngay cơ sở y tế gần nhất hoặc đến trực tiếp{" "}
+                        <span className="text-highlight">Hệ thống bệnh viện Đa khoa Piedmont</span>
+                    </li>
+                </div>
+            </div>
         </div>
     );
 };
