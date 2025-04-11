e6edbd27c3e066b3344a8f53c3df7c19affec379
ChiNg22nd04 <dedonai99@gmail.com>
2025-04-11 23:13:02 +0700

    update shift schedule

--- src/interfaces/profileStaff.tsx
+++ src/interfaces/profileStaff.tsx
@@ -1,6 +1,7 @@
 export interface ProfileStaff {
-    staff_id: number;
-    specialty_id: number;
-    clinic_id: number;
-    full_name: string;
+	staff_id: number;
+	specialty_id: number;
+	clinic_id: number;
+	full_name: string;
+	doctor_id: number;
 }
