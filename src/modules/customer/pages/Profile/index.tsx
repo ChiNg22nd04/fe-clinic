611f04cc1831cdcb7a56518774b73b512a225fef
ChiNg22nd04 <dedonai99@gmail.com>
2025-04-23 23:41:46 +0700

    update ui profile customer

--- src/modules/customer/pages/Profile/index.tsx
+++ src/modules/customer/pages/Profile/index.tsx
@@ -55,21 +55,18 @@ const Profile: React.FC = () => {
 
 	return (
 		<div className="content">
+			<h2>Thông tin cá nhân</h2>
 			<div className="profile-container">
-				<div className="profile-sidebar">
-					<div className="avatar-container">
-						<img
-							src={profile.image || "/default-avatar.png"} // Avatar fallback
-							alt="Profile"
-							className="avatar"
-						/>
-					</div>
-					<h3>{profile.fullName}</h3>
+				<div className="avatar-container">
+					<img
+						src={profile.image || "/default-avatar.png"} // Avatar fallback
+						alt="Profile"
+						className="avatar"
+					/>
 				</div>
 				<div className="profile-details">
-					<h2>Thông tin cá nhân</h2>
 					<div className="profile-item">
-						<strong>ID:</strong> {profile.id}
+						<strong>Họ và Tên:</strong> {profile.fullName}
 					</div>
 					<div className="profile-item">
 						<strong>Email:</strong> {profile.email}
@@ -78,31 +75,11 @@ const Profile: React.FC = () => {
 						<strong>Username:</strong> {profile.username}
 					</div>
 					<div className="profile-item">
-						<strong>Họ và Tên:</strong> {profile.fullName}
-					</div>
-					<div className="profile-item">
 						<strong>Số điện thoại:</strong> {profile.phone || "Chưa có"}
 					</div>
-					{/* <div className="profile-item">
-						<strong>Chức vụ:</strong> {roleText(profile.role)}
-					</div> */}
 					<div className="profile-item">
 						<strong>Trạng thái:</strong>{" "}
 						{profile.isActive ? "Đang hoạt động" : "Ngừng hoạt động"}
-					</div>
-					<div className="profile-item">
-						<strong>Đã xác minh:</strong>{" "}
-						{profile.isVerified ? "Đã xác minh" : "Chưa xác minh"}
-					</div>
-					{/* <div className="profile-item">
-						<strong>Ngày tạo:</strong>{" "}
-						{new Date(profile.createdAt).toLocaleDateString()}
-					</div> */}
-					<div className="profile-item">
-						<strong>Số lần OTP đã gửi:</strong> {profile.otpSentCount}
-					</div>
-					<div className="profile-item">
-						<strong>Mã xác minh:</strong> {profile.verificationCode || "Chưa có"}
 					</div>
 				</div>
 			</div>
